267c468ee55d0723b8e948c578b148e1583815a3
tinosaccoccia06-art <tinosaccoccia06@gmail.com>
2025-12-04 18:43:59 -0300

    Update repositorios.py

--- modelos/repositorios/repositorios.py
+++ modelos/repositorios/repositorios.py
@@ -1,12 +1,18 @@
-from modelos.repositorios.repositorio_Bebidas import RepositorioAnimales
+from modelos.repositorios.repositorio_animales import RepositorioAnimales
+from modelos.repositorios.repositorio_duenos import RepositorioDuenos
 
-
-animales = None
-
+_animales = None
+_duenos = None
 
 def obtenerRepoAnimales():
-    global animales
-    if animales == None:
-        animales = RepositorioAnimales()
-    return animales
+    global _animales
+    if _animales is None:
+        _animales = RepositorioAnimales()
+    return _animales
+
+def obtenerRepoDuenos():
+    global _duenos
+    if _duenos is None:
+        _duenos = RepositorioDuenos()
+    return _duenos
 
